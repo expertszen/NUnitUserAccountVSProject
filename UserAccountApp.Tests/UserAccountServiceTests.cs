d73cdba14aa168d2773e34056029e1b209b7443e
Developer1 <expertszen@gmail.com>
2025-07-09 12:16:40 +0530

    changes made as per the feedback on use case

--- UserAccountApp.Tests/UserAccountServiceTests.cs
+++ UserAccountApp.Tests/UserAccountServiceTests.cs
@@ -2,56 +2,10 @@
 using NUnit.Framework;
 using System;
 
-[TestFixture]
+
 public class UserAccountServiceTests
 {
     private UserAccountService _service;
 
-    [SetUp]
-    public void Init()
-    {
-        _service = new UserAccountService();
-    }
-
-    [TearDown]
-    public void Clean()
-    {
-        _service.Reset();
-    }
-
-    [Test, Category("Positive")]
-    public void Test_CreateUser_Success()
-    {
-        _service.CreateUser("john_doe");
-        Assert.AreEqual("john_doe", _service.GetUser("john_doe"));
-        Assert.IsFalse(_service.IsActive("john_doe"));
-    }
-
-    [Test, Category("Negative")]
-    public void Test_DuplicateUser_ShouldThrow()
-    {
-        _service.CreateUser("alice");
-        Assert.Throws<InvalidOperationException>(() => _service.CreateUser("alice"));
-    }
-
-    [Test, Category("Negative")]
-    public void Test_EmptyOrNullUsername_ShouldThrow()
-    {
-        Assert.Throws<ArgumentException>(() => _service.CreateUser(""));
-        Assert.Throws<ArgumentException>(() => _service.CreateUser(null));
-    }
-
-    [Test, Category("Positive")]
-    public void Test_ActivateUser_Success()
-    {
-        _service.CreateUser("dave");
-        _service.ActivateUser("dave");
-        Assert.IsTrue(_service.IsActive("dave"));
-    }
-
-    [Test, Category("Negative")]
-    public void Test_GetNonExistentUser_ShouldReturnNull()
-    {
-        Assert.IsNull(_service.GetUser("ghost"));
-    }
+    
 }
---
+++ output_screenshots/README.md
@@ -0,0 +1,2 @@
+# NUnitCalculatorProject
+Add all the screenshots of the project execution and Test Cases execution inside the folder - 'output_screenshots'.
